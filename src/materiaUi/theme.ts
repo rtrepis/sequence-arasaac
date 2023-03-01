3f05d3e0336b5cf3ff2a1095c46d01207590ce5d
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-03-01 11:32:28 +0100

    Added MaterialUi and theme

---
+++ src/materiaUi/theme.ts
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: "#31DC39",
+      main: "#1B7D20",
+      dark: "#175C1A",
+      contrastText: "#E3E3E3",
+    },
+    secondary: {
+      light: "#FAFAFA",
+      main: "#D9D9D9",
+      dark: "#A6A6A6",
+      contrastText: "#252525",
+    },
+  },
+});
+
+export default theme;
