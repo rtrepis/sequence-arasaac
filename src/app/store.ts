0e27e75618b5c2c29bcaa8798547c067eb97705e
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-03-03 16:07:28 +0100

    Added sequence slice and test and utils-test

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { sequenceReducer } from "./slice/sequenceSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    sequence: sequenceReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
