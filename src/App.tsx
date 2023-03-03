1c090bf73ed473c21775ff17f8ef1c73e8ff8003
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-03-03 16:13:48 +0100

    Removed unused component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { useSelector } from "react-redux";
 import "./App.css";
 import { RootState } from "./app/store";
 import PictogramCardList from "./components/PictogramCardList/PictogramCardList";
-import SequenceCreate from "./components/SequenceCreate/SequenceCreate";
 
 const App = () => {
   const sequence = useSelector((state: RootState) => state.sequence);
@@ -12,7 +11,6 @@ const App = () => {
   return (
     <Stack alignContent={"center"}>
       <h1 className="App">Sequence - AraSaac</h1>
-      <SequenceCreate />
       <PictogramCardList sequence={sequence} />
     </Stack>
   );
