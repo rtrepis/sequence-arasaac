3e86ce97b3c99f18db83946bd9d8e3b73a98a4b2
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-03-16 16:13:44 +0100

    Added funcionality and test

--- src/app/slice/sequenceSlice.test.tsx
+++ src/app/slice/sequenceSlice.test.tsx
@@ -2,6 +2,7 @@ import {
   PictogramI,
   ProtoPictogramI,
   SequenceI,
+  UpdatePictWordI,
   UpdateSettingItemI,
 } from "../../types/sequence";
 import { SettingItemPayloadI } from "../../types/ui";
@@ -12,13 +13,20 @@ import {
   applyAllSettingItemActionCreator,
   upDateSettingItemActionCreator,
   upDatePictNumberActionCreator,
+  upDatePictWordActionCreator,
 } from "./sequenceSlice";
 
 describe("Given the reducer sequenceSlice", () => {
-  let previousSequence: SequenceI = [{ index: 0, number: 0 }];
+  let previousSequence: SequenceI = [
+    { index: 0, number: 0, word: { keyWord: "" } },
+  ];
   describe("When call 'addPictogram' with pictogram payload", () => {
     test("Then should new state is same previous add new pictogram", () => {
-      const pictogram: PictogramI = { index: 1, number: 2220 };
+      const pictogram: PictogramI = {
+        index: 1,
+        number: 2220,
+        word: { keyWord: "" },
+      };
       const expectState = [...previousSequence, pictogram];
 
       const actionCreator = addPictogramActionCreator(pictogram);
@@ -39,15 +47,15 @@ describe("Given the reducer sequenceSlice", () => {
     });
   });
 
-  describe("When called 'upDatePictogram' with ProtoPictogramI", () => {
-    test("Then should new state with update PictogramSetting", () => {
+  describe("When called 'upDatePictNumber' with ProtoPictogramI", () => {
+    test("Then should new state with update pictogram of sequence", () => {
       const upDatePictogramNumber: ProtoPictogramI = {
         index: 0,
         number: 1254,
       };
-      previousSequence = [{ index: 0, number: 0 }];
+      previousSequence = [{ index: 0, number: 0, word: { keyWord: "" } }];
 
-      const expectState = [{ index: 0, number: 1254 }];
+      const expectState = [{ ...previousSequence[0], index: 0, number: 1254 }];
 
       const actionCreator = upDatePictNumberActionCreator(
         upDatePictogramNumber
@@ -58,14 +66,37 @@ describe("Given the reducer sequenceSlice", () => {
     });
   });
 
+  describe("When called 'upDatePictWord' with upDatePictWordI", () => {
+    test("Then should new state with update pictogram of sequence", () => {
+      const upDatePictogramWord: UpdatePictWordI = {
+        indexPict: 0,
+        word: { keyWord: "testWord" },
+      };
+      previousSequence = [{ index: 0, number: 0, word: { keyWord: "" } }];
+
+      const expectState = [
+        { ...previousSequence[0], index: 0, word: { keyWord: "testWord" } },
+      ];
+
+      const actionCreator = upDatePictWordActionCreator(upDatePictogramWord);
+      const newState = sequenceReducer(previousSequence, actionCreator);
+
+      expect(newState).toStrictEqual(expectState);
+    });
+  });
+
   describe("When called 'applyAllSettingItem' with applyAllPayload", () => {
     test("Then should new state change all property of pictogram", () => {
-      previousSequence = [{ index: 0, number: 0, skin: "default" }];
+      previousSequence = [
+        { index: 0, number: 0, skin: "default", word: { keyWord: "" } },
+      ];
       const applyAllPayload: SettingItemPayloadI = {
         item: "skin",
         value: "black",
       };
-      const expectState = [{ index: 0, number: 0, skin: "black" }];
+      const expectState = [
+        { ...previousSequence[0], index: 0, number: 0, skin: "black" },
+      ];
 
       const actionCreator = applyAllSettingItemActionCreator(applyAllPayload);
       const newState = sequenceReducer(previousSequence, actionCreator);
@@ -82,13 +113,13 @@ describe("Given the reducer sequenceSlice", () => {
         value: "asian",
       };
       previousSequence = [
-        { index: 0, number: 0, skin: "default" },
-        { index: 1, number: 0, skin: "black" },
+        { index: 0, number: 0, skin: "default", word: { keyWord: "" } },
+        { index: 1, number: 0, skin: "black", word: { keyWord: "" } },
       ];
 
       const expectState = [
-        { index: 0, number: 0, skin: "asian" },
-        { index: 1, number: 0, skin: "black" },
+        { ...previousSequence[0], index: 0, number: 0, skin: "asian" },
+        { ...previousSequence[1], index: 1, number: 0, skin: "black" },
       ];
 
       const actionCreator = upDateSettingItemActionCreator(
