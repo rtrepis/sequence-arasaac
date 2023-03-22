10c2b398bc8fdb26ce489fdf13f5f0a75911aacd
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-03-22 11:36:41 +0100

    Added language compiled and languages searchPictogram

--- src/languages/loadLocaleMessages.ts
+++ src/languages/loadLocaleMessages.ts
@@ -1,8 +1,11 @@
 const loadLocalMessages = (locale: string) => {
   switch (locale) {
+    case "ES":
+      return import("./es.json");
+    case "en-GB":
+      return import("./en.json");
     case "ca-ES":
       return import("./ca.json");
-
     default:
       return import("./en.json");
   }
