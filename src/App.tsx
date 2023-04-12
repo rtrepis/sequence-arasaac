63cd914226e9b17b8d1612dd8095008947ca8758
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-04-12 10:54:17 +0200

    Added languages in viewPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import "./App.css";
 import { useAppSelector } from "./app/hooks";
 import BarNavigation from "./components/BarNavigation/BarNavigation";
-import EditSequencesPages from "./pages/EditSequncesPages/EditSequencesPages";
+import EditSequencesPages from "./pages/EditSequencesPages/EditSequencesPages";
 import ViewSequencePage from "./pages/ViewSequencePages/ViewSequencePage";
 
 const App = () => {
-  const { view } = useAppSelector((state) => state.ui);
+  const { viewPage } = useAppSelector((state) => state.ui);
 
   return (
     <>
       <BarNavigation>
         <>
-          {!view && <EditSequencesPages />}
-          {view && <ViewSequencePage />}
+          {!viewPage && <EditSequencesPages />}
+          {viewPage && <ViewSequencePage />}
         </>
       </BarNavigation>
     </>
