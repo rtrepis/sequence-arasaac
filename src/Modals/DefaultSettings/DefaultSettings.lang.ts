a3011688dc196da6c04a47e6f8738fc6ce59506c
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-04-19 10:14:57 +0200

    Refactored forms pict edit and default settings

--- src/Modals/DefaultSettings/DefaultSettings.lang.ts
+++ src/Modals/DefaultSettings/DefaultSettings.lang.ts
@@ -11,6 +11,11 @@ const messages = defineMessages({
     defaultMessage: "close",
     description: "close Settings Default ",
   },
+  pictGuide: {
+    id: "components.defaultSettings.pictGuide",
+    defaultMessage: "Pictogram guide",
+    description: "Pictogram pictogram for default settings",
+  },
 });
 
 export default messages;
---
+++ src/components/ViewSequencesSettings/ViewSequenceSettings.styled.ts
@@ -0,0 +1,13 @@
+import { SxProps } from "@mui/material";
+
+export const tab: SxProps = {
+  display: "flex",
+  width: 100,
+  height: 40,
+  backgroundColor: "green",
+  borderTopLeftRadius: 20,
+  borderTopRightRadius: 20,
+  textAlign: "center",
+  justifyContent: "center",
+  alignItems: "center",
+};
