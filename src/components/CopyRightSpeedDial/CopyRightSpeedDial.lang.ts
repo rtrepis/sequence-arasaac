dd6e7d6a7b59cb0d7b21a46d11b45811657b2c50
Ramon Trepat Burgués <r.trepis@gmail.com>
2023-08-31 07:49:09 +0200

    Add languages in speed dial

---
+++ src/components/CopyRightSpeedDial/CopyRightSpeedDial.lang.ts
@@ -0,0 +1,17 @@
+import { defineMessages } from "react-intl";
+
+const messages = defineMessages({
+  license: {
+    id: "components.copyRightDial.license.label",
+    defaultMessage:
+      "Author of the pictograms: Sergio Palao.\n Origen: ARASAAC (http://www.arasaac.org). License: CC (BY-NC-SA).",
+    description: "license ",
+  },
+  auth: {
+    id: "components.copyRightDial.auth.label",
+    defaultMessage: `Design & all right reserved: Ramon Trepat & AraSaac. seqsaac@gmail.com`,
+    description: "Page auth",
+  },
+});
+
+export default messages;
