f8591693483f209b559b32f1fc7205bb901a98ec
Ramon Trepat Burgués <r.trepis@gmail.com>
2026-02-07 15:23:57 +0100

    Hot fixed ara saac fittgerald

--- src/data/fitzgeraldColors.ts
+++ src/data/fitzgeraldColors.ts
@@ -1,9 +1,10 @@
 const fitzgeraldColors = {
-  person: "#DD8800",
-  verb: "#229900",
-  adjective: "#0088CC",
-  expression: "#CC00BB",
-  not: "#333333",
+  1: { value: "properNoun", color: "#FFEB3B" },
+  2: { value: "noun", color: "#FF9800" },
+  3: { value: "verb", color: "#4CAf50" },
+  4: { value: "descriptive", color: "#2196F3" },
+  5: { value: "socialContent", color: "#9C27b0" },
+  6: { value: "miscellaneous", color: "#FFFFFF" },
 };
 
 export default fitzgeraldColors;
