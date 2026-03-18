2cfa5eaf8cf3268ade89440ff5c9c3ba2fe419a5
Ramon Trepat Burgués <r.trepis@gmail.com>
2026-03-18 22:15:08 +0100

    Added sequences translated

---
+++ src/pages/EditSequencesPage/EditSequencesPage.lang.ts
@@ -0,0 +1,9 @@
+import { defineMessages } from "react-intl";
+
+export const messages = defineMessages({
+  sequences: {
+    id: "view.editSequences.sequences.label",
+    defaultMessage: "Sequences",
+    description: "Etiqueta del títol de la secció de seqüències a l'editor",
+  },
+});
